a26806cfb51d89ccb85a058a8d390c560160106f
rameshuconn <ramesh.uconn@gmail.com>
2024-02-20 18:00:56 -0500

    Finish layout and routes

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,4 +9,16 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
   end
+
+  test "help links" do
+    # e.g. start on this webpage
+    get help_path
+    # ensure that this is the proper URL for this webpage
+    assert_template 'static_pages/help'
+    # Then check that the following URLs work from this webpage
+    assert_select "a[href=?]", root_path
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+  end  
 end
